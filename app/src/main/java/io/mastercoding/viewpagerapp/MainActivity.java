b8be1603cb1d2e1d5708979ad5ac7bb8f17103f6
Ayush Shukla <ayushsukla321@gmail.com>
2026-02-07 23:28:08 +0530

    Initial commit: ViewPagerApp with TabLayout and ViewPager2

---
+++ app/src/main/java/io/mastercoding/viewpagerapp/MainActivity.java
@@ -0,0 +1,60 @@
+package io.mastercoding.viewpagerapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewpager;
+    MyViewPagerAdapter myAdapter;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        myAdapter = new MyViewPagerAdapter(
+                getSupportFragmentManager(),
+                getLifecycle()
+        );
+
+        myAdapter.addFragment(new Fragment1());
+        myAdapter.addFragment(new Fragment2());
+        myAdapter.addFragment(new Fragment3());
+
+        viewpager = findViewById(R.id.viewPager2);
+        viewpager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewpager.setAdapter(myAdapter);
+
+        new TabLayoutMediator(tabLayout, viewpager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int i) {
+                        tab.setText("Fragment " + (i + 1));
+                    }
+                }
+        ).attach();
+
+    }
+}
